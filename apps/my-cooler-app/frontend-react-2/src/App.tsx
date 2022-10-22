5416ed2164fb1cf0d4111dc7f19171e5112415f9
Sam <samkio@users.noreply.github.com>
2022-10-22 21:21:20 +0000

    Add my-cooler-app

---
+++ apps/my-cooler-app/frontend-react-2/src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ExampleType } from "shared";
+import { dynamicConfigUrl } from "./config";
+
+function App() {
+  // Very basic dependency to verify depenent local packages are built.
+  const value: ExampleType = {
+    numberField: 123,
+    stringField: "exampleValue",
+  };
+
+  console.log({
+    ...value,
+    environmentVariable: process.env.REACT_APP_ENVIRONMENT_VARIABLE,
+  });
+ 
+  fetch(dynamicConfigUrl)
+    .then((i) => i.json())
+    .then((i) => console.log(i));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
